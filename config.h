f4d0480d4d7e8e68a7d37ae250640c782319c724
Alex Kritikos <alex@8bitb.us>
2012-08-23 20:59:01 -0500

    Allow fractional fonts

--- config.h
+++ config.h
@@ -46,7 +46,7 @@
 // #define DEFAULT_DIRECTORY      g_get_current_dir()
 // #define DEFAULT_TERMINAL_SIZE  80x24
 // #define EXPORT_WINDOWID        TRUE
-#define FONT                   "AK Sans Mono 8"
+#define FONT                   "AK Sans Mono 7.5"
 // #define FONT_ANTI_ALIAS        TRUE
 // #define FONT_ENABLE_BOLD_TEXT  TRUE
 // #define MOUSE_CTRL_SATURATION  TRUE  /* Scroll left or right to adjust it  */
